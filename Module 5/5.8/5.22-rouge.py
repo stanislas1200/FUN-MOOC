00d1a4caeb828c5c47752f42c296c0519a7638b3
stanislas1200 <godinstanislas1@Gmail.com>
2022-10-17 20:50:02 +0200

    Update

--- Module 5/5.8/5.22-rouge.py
+++ Module 5/5.8/5.22-rouge.py
@@ -1,23 +1,14 @@
-import string
-
 def wc(file):
     """Retourne un tuple qui contient le nombre de mots, de caractères et de lignes du fichier"""
     nbr_word = 0
     nbr_char = 0
     nbr_line = 0
-    lst = []
     with open(file, encoding="utf-8") as f:
         for line in f:
             nbr_line += 1
             for i in range(len(line)):
-                if not line[i].isalnum() and line[i] not in lst:
-                    lst += line[i]
                 if line[i].isalnum:
                     nbr_char += 1
                 if not line[i].isalnum() and line[i-1].isalnum():
                     nbr_word += 1
-    print(lst)
-    return (nbr_char, nbr_word, nbr_line)
-
-print(wc('Apollinaire2.txt'))
-## Line good, but not word and char
+    return nbr_char, nbr_word, nbr_line
