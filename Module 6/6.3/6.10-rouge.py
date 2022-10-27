c27afc96be634de54e79dfb9b81d9fd94cef5890
stanislas1200 <godinstanislas1@Gmail.com>
2022-10-27 13:14:00 +0200

    New 100%

--- Module 6/6.3/6.10-rouge.py
+++ Module 6/6.3/6.10-rouge.py
@@ -1,5 +1,3 @@
 def valeurs(dico):
     """Return a liste of the value of a dictionary sorted by the key"""
     return [dico[key] for key in sorted(dico.keys())]
-
-print(valeurs({'three': 'trois', 'two': 'deux', 'one': 'un'}))
